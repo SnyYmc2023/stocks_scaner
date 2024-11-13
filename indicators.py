60ab251c350d1df6519e69610299b8dc0e73a30f
SinanYMC <sinan.yeatman@gmail.com>
2024-11-13 23:23:02 +0300

    re test 2

--- indicators.py
+++ indicators.py
@@ -18,3 +18,19 @@ def calculate_hma(data, period):
     full_period_wma = calculate_wma(data, period)
     hma_data = 2 * half_period_wma - full_period_wma
     return calculate_wma(hma_data, int(np.sqrt(period)))  # Son adımda karekök periyodla WMA
+
+def check_ema_crossover_near(data, short_period, long_period, threshold):
+    """EMA kesişimi ve kesişime yakınlık belirler."""
+    data['EMA_Short'] = calculate_ema(data['close'], short_period)
+    data['EMA_Long'] = calculate_ema(data['close'], long_period)
+    data['BullishCross'] = (data['EMA_Short'] > data['EMA_Long']) & (data['EMA_Short'].shift(1) <= data['EMA_Long'].shift(1))
+    data['NearCross'] = (data['BullishCross'] == False) & (abs(data['EMA_Short'] - data['EMA_Long']) <= threshold)
+    return data
+
+def check_wma_crossover_near(data, short_period, long_period, threshold):
+    """WMA kesişimi ve kesişime yakınlık belirler."""
+    data['WMA_Short'] = calculate_wma(data['close'], short_period)
+    data['WMA_Long'] = calculate_wma(data['close'], long_period)
+    data['BullishCross'] = (data['WMA_Short'] > data['WMA_Long']) & (data['WMA_Short'].shift(1) <= data['WMA_Long'].shift(1))
+    data['NearCross'] = (data['BullishCross'] == False) & (abs(data['WMA_Short'] - data['WMA_Long']) <= threshold)
+    return data
