1d2a31505d269304a37fac5cd0d987afaec2f95b
SinanYMC <sinan.yeatman@gmail.com>
2024-11-14 16:31:29 +0300

    4

--- display.py
+++ display.py
@@ -4,10 +4,11 @@ import pandas as pd
 def display_results(df, title, max_results=None):
     """Sonuçları tablo formatında terminalde görüntüler ve Telegram için tablo oluşturur."""
     if not df.empty:
-        # Volume değerlerini bindelik ayraçla biçimlendir
+        # Volume değerlerini sayısal değere çevirme ve bindelik ayraçla biçimlendirme
         if 'Volume' in df.columns:
-            df['Volume'] = pd.to_numeric(df['Volume'], errors='coerce').fillna(0).astype(int)
-            df['Volume'] = df['Volume'].apply(lambda x: f"{x:,.0f}")
+            # Sadece sayısal olmayanları kontrol etmek ve sıfır olarak doldurmak için dönüştürme
+            df['Volume'] = pd.to_numeric(df['Volume'], errors='coerce').fillna(-1)
+            df['Volume'] = df['Volume'].apply(lambda x: f"{x:,.0f}" if x != -1 else "N/A")  # Hatalı veriler "N/A" olarak işaretlenir.
         
         # Sadece max_results kadar sonuç göstermek için sıralama
         if max_results:
